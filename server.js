ad4b9722cc991cffbaaa6b15adce244e80cd4472
Wafa Mohessin <wafamohessin@gmail.com>
2021-12-13 22:26:05 -0500

    update employee

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const prompts = () => {
             createEmployee();
             break;
           case "Update Employee Role":
-            employeeUpdat();
+            employeeUpdate();
            break;
           case "Quit":
             process.exit();
@@ -131,57 +131,55 @@ function createRole() {
   });
 }
 
-function createEmployee() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the first name and the last name of the employee?",
-      },
-    ])
-    .then((answers) => {
-      let name = answers;
-      db.addEmployee(name)
-        .then(() => console.log(`Added ${name.name} to the database`))
-        .then(() => prompts());
-    });
-}
 
-function addEmployee() {
-   
-  inquirer
-    .prompt([
-              {
-                type: 'input',
-                name: 'first_name',
-                message: `What is the employee's first name?`,
-              },
-             {
-                 type: 'input',
-                 name: 'last_name',
-                 message: `What is the employee's last name?`,
-             },
-             {
+function createEmployee() {
+  db.findAllRoles().then(([data]) => {
+    const roleChoices = data.map(({ id, name }) => ({
+      name: name,
+      value: id,
+
+    }))
+    db.findAllEmployees().then(([data]) => {
+      const managerChoices = data
+      //.filter(({role}) => role == "manager")
+      .map(({ id, name }) => ({
+        name: name,
+        value: id,
+      }))
+      inquirer
+      .prompt([
+                {
+                  type: 'input',
+                  name: 'first_name',
+                  message: `What is the employee's first name?`,
+                },
+               {
+                   type: 'input',
+                   name: 'last_name',
+                   message: `What is the employee's last name?`,
+               },
+               {
+                   type: 'list',
+                   name: 'role_id',
+                   message: `What is the employee's role?`,
+                   choices: roleChoices,
+               },
+               /* {
                  type: 'list',
-                 name: 'role',
-                 message: `What is the employee's role?`,
-                 choices: roles,
-             },
-             {
-               type: 'list',
-               name: 'manager',
-               message: `Who is the employee's manager?`,
-               choices: managers,
-             },
-        ])
-      
-    .then((answers) => {
-      let name = answers;
-      db.addEmployee(name)
-        .then(() => console.log(`Added ${name.name} to the database`))
-        .then(() => prompts());
+                 name: 'manager',
+                 message: `Who is the employee's manager?`,
+                 choices: managerChoices,
+               }, */
+          ])
+        
+      .then((answers) => {
+        db.addEmployee(answers)
+          .then(() => console.log(`Added ${answers.first_name} to the database`))
+          .then(() => prompts());
+      }) 
     })
+  })
+}
 
 function employeeUpdate (){
   
@@ -189,24 +187,23 @@ function employeeUpdate (){
     .prompt([
             {
                 type: 'list',
-                name: 'employee',
+                name: 'employee_id',
                 message: `Which employee's role do you want to update?`,
                 choices: employees
              },
             {
                 type: 'list',
-                name: 'role',
+                name: 'role_id',
                 message: `Which role do you want to assign the selected employee?`,
                 choices: roles
             }
         ])
 
     .then((answers) => {
-      let name = answers;
-      db.addEmployee(name)
-        .then(() => console.log(`Added ${name.name} to the database`))
+      db.updateEmployee(answers.role_id, answer.employee_id)
+        .then(() => console.log(`Updated ${answers.employee_id} to the database`))
         .then(() => prompts());
     });
 }
-}
+
 prompts();
